f2806d2ee6094d81a6636780cfb2c833beb978d7
SzymonSliwa <creedo@tlen.pl>
2023-05-17 22:50:18 +0200

    full app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import { Profile } from './Profile/profile';
+import { Statistics } from './Statistics/statistics';
+import { FriendList } from './FriendList/friendList';
+import { TransactionHistory } from './TransactionHistory/transactions';
+import data from './data';
+import friends from './friends';
+import transactions from './transactions';
+import user from './user';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="container">
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />;
+      <TransactionHistory transactions={transactions} />
     </div>
   );
 };
