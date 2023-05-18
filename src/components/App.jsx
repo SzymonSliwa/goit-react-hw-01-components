2191e0d1df251d6baf11d94fb6557e81176f4989
SzymonSliwa <creedo@tlen.pl>
2023-05-18 21:05:35 +0200

    trying to fix module not found bug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
-import { Profile } from './Profile/Profile.jsx';
-import { Statistics } from './Statistics/Statistics';
-import { FriendList } from './FriendList/FriendList';
-import { TransactionHistory } from './TransactionHistory/Transactions';
 import data from '../data/data';
 import friends from '../data/friends';
 import transactions from '../data/transactions';
 import user from '../data/user';
+
+import { Statistics } from './Statistics/Statistics';
+import { Profile } from './Profile/Profile.jsx';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/Transactions';
 
 export const App = () => {
   return (
